1d28ecddf3ee8776f15c941b701990a490a73039
NastyaSiya <rumyantseva11aa@gmail.com>
2023-05-05 19:04:45 +0300

    Domashka na 05.05.2023

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -5,10 +5,7 @@ from pages.elements_page import ElementsPage
 def test_text(browser):
     demo_qa_hw = DemoQa(browser)
     demo_qa_hw.visit()
-    if demo_qa_hw.podval.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.':
-        return True
-    else:
-        return False
+    assert demo_qa_hw.podval.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
 
 
 def test_btn_text(browser):
@@ -16,7 +13,4 @@ def test_btn_text(browser):
     demo_qa_hw2 = ElementsPage(browser)
     demo_qa_hw1.visit()
     demo_qa_hw1.btn_hw.click()
-    if demo_qa_hw2.nadpis_tsentr.get_text() == 'Please select an item from left to start practice.':
-        return True
-    else:
-        return False
+    assert demo_qa_hw2.nadpis_tsentr.get_text() == 'Please select an item from left to start practice.'
